b335b604b0c38555c1b45fb6fe39388996da9dc9
mostafamoh1 <153465034+mostafamoh1@users.noreply.github.com>
2024-01-05 23:18:17 +0200

    Create interactionCreate.js

---
+++ interactionCreate.js
@@ -0,0 +1,21 @@
+const { prefix, prefixalt } = require('../config.json');
+
+module.exports = {
+  name: 'interactionCreate',
+  once: false,
+  async execute(client, interaction) {
+    if (!interaction.isCommand()) return;
+    if (!interaction.channel.guild) return;
+    
+    const command = client.slashCommands.get(interaction.commandName);
+
+    if (!command) return ;
+
+    try {
+      await command.execute(client, interaction);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: `There was an error while executing this command!:\n${error}` });
+    }
+  }
+  
