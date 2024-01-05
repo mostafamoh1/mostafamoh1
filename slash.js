b049e7dfe7cb5ea2c914cf5459f1ecfe8d15a7ad
mostafamoh1 <153465034+mostafamoh1@users.noreply.github.com>
2024-01-05 23:26:07 +0200

    Create slash.js

---
+++ slash.js
@@ -0,0 +1,25 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const fs = require('fs');
+const ascii = require('ascii-table');
+let table = new ascii(`(/) SlashCommands`);
+table.setHeading('🔸Commands', '🔸Load Status');
+module.exports = (client) => {
+fs.readdirSync('./SlashCommands').forEach((folder) => {
+    const commandFiles = fs.readdirSync(`./SlashCommands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+    const command = require(`../SlashCommands/${folder}/${file}`);
+    if (command.name) {
+        client.slashCommands.set(command.name, command);
+        table.addRow(`${file}`, '✔');
+    } else {
+        table.addRow(`${file}`, '✖');
+        continue;
+    }
+    }
+    });
+    const commands = client.slashCommands.map(({ execute, ...data }) => data);
+    console.log((table.toString()).blue);
+    console.log('Started refreshing slash commands...');
+    console.log(`Successfully reloaded ${commands.length} slash commands!`);
+}
